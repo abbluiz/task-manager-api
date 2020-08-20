a60b6eb6de15484aa59690f8104ca5e364379fc9
LABB <unt@labb.email>
2020-08-20 12:10:30 -0400

    Starts using Jest

---
+++ tests/math.test.js
@@ -0,0 +1,29 @@
+const { calculateTip, fahrenheitToCelsius, celsiusToFahrenheit } = require('../src/math');
+
+test('Should calculate total with tip', () => {
+
+    const total = calculateTip(10, .3);
+    expect(total).toBe(13);
+
+});
+
+test('Should calculate total with default tip (13%)', () => {
+
+    const total = calculateTip(10);
+    expect(total).toBe(11.3);
+
+});
+
+test('Should convert 32 F to 0 C', () => {
+
+    const result = fahrenheitToCelsius(32);
+    expect(result).toBe(0);
+
+});
+
+test('Should convert 0 C to 32 F', () => {
+
+    const result = celsiusToFahrenheit(0);
+    expect(result).toBe(32);
+
+});
